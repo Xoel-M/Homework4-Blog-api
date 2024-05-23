2390122bd25241b0a782509da787b5ee6e70363e
Xoel-M <targetjoel77@gmail.com>
2024-05-23 13:54:56 -0500

    added function in form.js to store in local storage

---
+++ assets/js/form.js
@@ -0,0 +1,18 @@
+const form = document.getElementById('blogForm');
+const userNameInput = document.querySelector('#user-name');
+const titleInput = document.querySelector('#title');
+const contentInput = document.querySelector('#content');
+
+function handleSubmit(event) {
+    event.preventDefault();
+
+    const blogData = {
+        userName: userNameInput.value.trim(),
+        title: titleInput.value.trim(),
+        content: contentInput.value.trim(),
+    }; 
+
+    localStorage.setItem('Data', JSON.stringify(blogData));  
+}
+
+form.addEventListener('submit', handleSubmit)
