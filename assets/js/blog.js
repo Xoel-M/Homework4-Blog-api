274b764dcad43d6fbee6cb19740d0db283e30326
Xoel-M <targetjoel77@gmail.com>
2024-05-23 16:05:27 -0500

    added function for go back button in blog.js

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,6 +1,11 @@
 function toggleMode() {
     const body = document.body;
     body.classList.toggle('dark-mode');
-  }
+}
+
+function goBack() {
+    window.location.href = 'index.html';
+}
 
 document.getElementById('modeToggle').addEventListener('change', toggleMode);
+document.getElementById('backButton').addEventListener('click', goBack);
